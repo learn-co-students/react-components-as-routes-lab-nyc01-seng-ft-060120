cf4ca1d92bcc764c3c0e9803bb996311f17fe408
berellevy <berellevy@gmail.com>
2020-08-11 18:17:27 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,22 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {
+        actors.map(({name, movies}, index) => (
+          <div key={index} >
+            <h3>Name: {name}</h3>
+            <p>Movies:</p>
+            <ul>
+              {
+                movies.map((movie, index) => (
+                  <li key={index}>{movie}</li>
+                ))
+              }
+            </ul>
+          </div>
+        ))
+      }
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,22 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {
+        directors.map(({name, movies}, index) => (
+          <div key={index} >
+            <h3>Name: {name}</h3>
+            <p>Movies:</p>
+            <ul>
+              {
+                movies.map((movie, index) => (
+                  <li key={index}>{movie}</li>
+                ))
+              }
+            </ul>
+          </div>
+        ))
+      }
     </div>
   );
 }
