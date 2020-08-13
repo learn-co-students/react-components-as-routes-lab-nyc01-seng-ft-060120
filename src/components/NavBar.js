b4c655309817f3aeac64d78b9755153ee0e0da0f
Bearinawolfpack <bearinawolfpack@gmail.com>
2020-08-12 20:13:43 -0400

    Done.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,14 @@ import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <NavLink to="/" activeStyle={{background: 'pink'}}>Home</NavLink>
+      <NavLink to="/movies" activeStyle={{background: 'pink'}}>Movies</NavLink>
+      <NavLink to="/actors" activeStyle={{background: 'pink'}}>Actors</NavLink>
+      <NavLink to="/directors" activeStyle={{background: 'pink'}}>Directors</NavLink>
     </div>
   );
 };
 
 export default NavBar;
+
+// Movies, Actors, Directors
